731da4be1ce84bc5d939cdb736bca00c76456941
bubbly853 <bubbly853@gmail.com>
2025-11-20 16:22:16 -0600

    Update urls.py

--- helmsman/urls.py
+++ helmsman/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
+import django_cas_ng.views as cas_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # CAS authentication URLs
-    path('accounts/', include('django_cas_ng.urls')),
-    
-    # Home page (protected by login)
+    path('accounts/login/', cas_views.LoginView.as_view(), name='cas_ng_login'),
+    path('accounts/logout/', cas_views.LogoutView.as_view(), name='cas_ng_logout'),
+    path('accounts/callback/', cas_views.CallbackView.as_view(), name='cas_ng_proxy_callback'),
     path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
 ]
