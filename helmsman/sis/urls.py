8e2368c5f8918000bf81a679e927456a41994586
bubbly853 <bubbly853@gmail.com>
2025-11-21 13:08:09 -0600

    Create urls.py

---
+++ helmsman/sis/urls.py
@@ -0,0 +1,17 @@
+"""
+SIS URL Configuration
+Place this in /srv/ribbon2helmsman/helmsman/sis/urls.py
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'sis'
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('students/', views.student_list, name='student_list'),
+    path('students/<str:student_id>/', views.student_detail, name='student_detail'),
+    path('courses/', views.course_list, name='course_list'),
+    path('courses/<str:course_id>/', views.course_detail, name='course_detail'),
+]
