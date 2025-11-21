c0d15ea5244448e8806fd2a9bc22d15a3f664c25
bubbly853 <bubbly853@gmail.com>
2025-11-21 13:21:49 -0600

    Update urls.py

--- helmsman/helmsman/urls.py
+++ helmsman/helmsman/urls.py
@@ -4,9 +4,8 @@ Place this file in /srv/ribbon2helmsman/helmsman/helmsman/urls.py
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,6 +18,6 @@ urlpatterns = [
          auth_views.LogoutView.as_view(),
          name='logout'),
     
-    # Redirect root to a default view (customize as needed)
-    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+    # SIS application URLs
+    path('', include('sis.urls')),
 ]
