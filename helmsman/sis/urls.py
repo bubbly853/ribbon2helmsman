b24e436044ff63eeb2d035889b7026d2d94ad79e
bubbly853 <bubbly853@gmail.com>
2026-01-16 10:44:07 -0600

    Update urls.py

--- helmsman/sis/urls.py
+++ helmsman/sis/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('students/', views.student_list, name='student_list'),
     path('students/<str:student_rbid>/', views.student_detail, name='student_detail'),
+    path('persons/', views.person_list, name='person_list'),
+    path('persons/<str:student_rbid>/', views.person_detail, name='person_detail'),
     path('courses/', views.course_list, name='course_list'),
     path('courses/<str:course_id>/', views.course_detail, name='course_detail'),
 ]
