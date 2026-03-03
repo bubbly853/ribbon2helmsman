c7e374d6d670bb6f8c45ef89b1690c24159fcb32
bubbly853 <bubbly853@gmail.com>
2026-03-03 10:10:10 -0600

    Update models.py

--- helmsman/sis/models.py
+++ helmsman/sis/models.py
@@ -581,7 +581,7 @@ class HsvStdnt(models.Model):
     hsv_stdnt_birthday = models.DateField(blank=True, null=True, db_comment='Date of birth.')
     hsv_stdnt_lvid = models.CharField(max_length=2, db_comment='Student level ID.')
     hsv_stdnt_level = models.CharField(max_length=32, db_comment='Student level human-readable name.')
-    hsv_stdnt_stud = models.CharField(max_length=2, db_comment='Student type ID.')
+    hsv_stdnt_stid = models.CharField(max_length=2, db_comment='Student type ID.')
     hsv_stdnt_student_type = models.CharField(max_length=32, db_comment='Student type human-readable name.')
     hsv_stdnt_mrid = models.CharField(max_length=8, blank=True, null=True, db_comment='Major ID.')
     hsv_stdnt_major = models.CharField(max_length=64, blank=True, null=True, db_comment='Major human-readable name.')
