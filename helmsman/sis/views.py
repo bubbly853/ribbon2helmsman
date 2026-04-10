f0ac9fcd0e96fb70c89d28c90514aaf765fff51f
Jonathan Petruska <petruska@nsuok.edu>
2026-04-10 15:11:52 -0500

    fix

--- helmsman/sis/views.py
+++ helmsman/sis/views.py
@@ -1176,7 +1176,8 @@ def term_list(request):
 def term_detail(request, term_tmid):
     """List terms with search and pagination"""
     term = SglTerms.objects.using('sis').filter(sgl_terms_tmid=term_tmid).first()
-    get_object_or_404(term)
+    if not ident:
+        return get_object_or_404(term)
     fyears = FglFyear.objects.using('sis').all().order_by('fgl_fyear_fyid')
 
     context = {
