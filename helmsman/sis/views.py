186882ed25d941186334f4b366a6090c2efd1d2f
Jonathan Petruska <petruska@nsuok.edu>
2026-04-09 23:20:32 -0500

    fixed missing column

--- helmsman/sis/views.py
+++ helmsman/sis/views.py
@@ -1178,7 +1178,7 @@ def curriculum_audit(request, stucv_scid):
     ident=GumIdent.objects.using('sis').filter(gum_ident_rbid=stucv.scm_stucv_rbid_id).first()
     creqs=ScrCreqs.objects.using('sis').filter(scr_creqs_cvid_id=stucv.scm_stucv_cvid_id).all().order_by('scr_creqs_crid')
     rqgrp=ScrRqgrp.objects.using('sis').filter(scr_rqgrp_cvid_id=stucv.scm_stucv_cvid_id).all().order_by('scr_rqgrp_hr_name')
-    audit=HsvAudit.objects.using('sis').filter(hsv_audit_scid=stucv_scid).all().order_by('hsv_audit_crid')
+    audit=HsvAudit.objects.using('sis').filter(hsv_audit_scid=stucv_scid).all().order_by('hsv_audit_sbid', 'hsv_audit_crse_numb')
     context = {
         'stucv': stucv,
         'ident': ident,
