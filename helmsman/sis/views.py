59b4ab7bcc9391be7e2703958f6c3b47dafee398
Jonathan Petruska <petruska@nsuok.edu>
2026-04-17 08:12:06 -0500

    course create added to dashboard

--- helmsman/sis/views.py
+++ helmsman/sis/views.py
@@ -551,7 +551,8 @@ def dashboard(request):
         {"url": reverse("sis:section_create"), "title": "Create Section",  "desc": "Create a new section record",  "icon": "📅✚"},
         {"url": reverse("sis:enrollment_create_student_select"), "title": "Create Enrollment",  "desc": "Enroll a student in a section",  "icon": "👨‍🎓📅✚"},
         {"url": reverse("sis:student_create_person_select"), "title": "Create Student Term Record",  "desc": "Create a term record for a student, creates a student record if needed",  "icon": "👨‍🎓✚"},
-        {"url": reverse("sis:term_create"), "title": "Create Term",  "desc": "Create a term",  "icon": "🗓️"},
+        {"url": reverse("sis:term_create"), "title": "Create Term",  "desc": "Create a term",  "icon": "🗓️✚"},
+        {"url": reverse("sis:course_create"), "title": "Create Course",  "desc": "Create a course",  "icon": "🗓️✚"},
         {"url": reverse("sis:marks_enter_section_select"), "title": "Enter or Update Final Marks",  "desc": "Enter or update final marks for a section",  "icon": "✅📝"},
         {"url": "/admin/", "title": "Admin",  "desc": "Django administration panels",  "icon": "⚙️"},
     ]
